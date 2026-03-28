717e1490aea3ea5249dea97e80e934cbb6580a3b
hkdigiverse2108 <hkdigiversework@gmail.com>
2026-03-28 15:02:15 +0530

    policy changes code

--- src/Routes/policy.ts
+++ src/Routes/policy.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { policyController } from "../controllers";
+
 const router = Router();
+
 router.post("/add", policyController.add_policy);
 router.put("/edit", policyController.edit_policy);
 router.get("/all", policyController.get_policy_by_type);
+
 export const policyRouter = router;

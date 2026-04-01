18173ae8c25a7fc8df12f9de381f143c5b4e1f13
HK-Bhupendra <bhupendrahkdigiverse@gmail.com>
2026-04-01 10:15:20 +0530

    changed newsletter isactive feature

--- src/database/models/newsletter.ts
+++ src/database/models/newsletter.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 
 const newsletterSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    isActive: { type: Boolean, default: true },
     isDeleted: { type: Boolean, default: false },
 }, { timestamps: true });
 

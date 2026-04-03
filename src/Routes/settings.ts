c901f62d1dfb61a1ab9c5224f1c637b61cb69da0
HK-ankithkdigiverse <ankithkdigiverse@gmail.com>
2026-04-03 18:42:24 +0530

    settinsg

--- src/Routes/settings.ts
+++ src/Routes/settings.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { settingsController } from "../controllers";
-import { adminJWT, userJWT } from "../helper";
+import { adminJWT } from "../helper";
 
 const router = Router();
 
 router.put("/update", adminJWT, settingsController.update_settings);
-router.get("/", userJWT, settingsController.get_settings);
+router.get("/", settingsController.get_settings);
 
 export const settingsRouter = router;

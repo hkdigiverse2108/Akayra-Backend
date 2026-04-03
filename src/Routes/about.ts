a2817a5878536e413f51887d3dc91e056f114225
HK-ankithkdigiverse <ankithkdigiverse@gmail.com>
2026-04-03 15:34:44 +0530

    about

--- src/Routes/about.ts
+++ src/Routes/about.ts
@@ -8,6 +8,6 @@ router.post("/add",adminJWT, aboutController.add_about_section);
 router.put("/edit",adminJWT, aboutController.edit_about_section_by_id);
 router.delete("/:id",adminJWT, aboutController.delete_about_section_by_id);
 router.get("/all", aboutController.get_all_about_sections);
-router.get("/:id",userJWT, aboutController.get_about_section_by_id);
+router.get("/:id", aboutController.get_about_section_by_id);
 
 export const aboutRouter = router;

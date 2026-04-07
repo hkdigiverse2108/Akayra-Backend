41ba9856d1143639429e373f858889dbfd108f0d
hk-krish <krishhkdigiverse@gmail.com>
2026-04-07 10:22:49 +0530

    feat: add address management routes with user authentication middleware

--- src/Routes/address.ts
+++ src/Routes/address.ts
@@ -4,9 +4,9 @@ import { addressController } from "../controllers";
 
 const router = Router();
 
-router.post("/add",adminJWT , addressController.add_address);
-router.put("/edit",adminJWT, addressController.edit_address);
-router.delete("/:id",adminJWT, addressController.delete_address);
-router.get("/all",adminJWT, addressController.get_all_address);
+router.post("/add", userJWT, addressController.add_address);
+router.put("/edit", userJWT, addressController.edit_address);
+router.delete("/:id", userJWT, addressController.delete_address);
+router.get("/all", userJWT, addressController.get_all_address);
 
 export const addressRouter = router;

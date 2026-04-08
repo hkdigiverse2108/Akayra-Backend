a3f892ae9d77acfc7f961b504fefc0c30fb1be43
HK-ankithkdigiverse <ankithkdigiverse@gmail.com>
2026-04-08 10:53:21 +0530

    wishist

--- src/Routes/wishlist.ts
+++ src/Routes/wishlist.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { wishlistController } from "../controllers";
-import { adminJWT } from "../helper";
+import { userJWT } from "../helper";
 
 const router = Router();
 
-router.post("/add",adminJWT , wishlistController.add_to_wishlist);
-router.delete("/:id",adminJWT, wishlistController.remove_from_wishlist);
-router.get("/",adminJWT, wishlistController.get_my_wishlist);
+router.post("/add",userJWT , wishlistController.add_to_wishlist);
+router.delete("/:id",userJWT, wishlistController.remove_from_wishlist);
+router.get("/",userJWT, wishlistController.get_my_wishlist);
 
 export const wishlistRouter = router;
